24e66244e13cdfc66b66111afa6d652c22e685c0
oshagara <oshagaras@gmail.com>
2024-01-25 22:24:48 -0500

    changes made from theia lab environment

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,21 +1,28 @@
-import { React, useState } from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-    const [name, setName] = useState('');
+  const {dispatch } = useContext(AppContext);
 
-    const currencytype = {
-        name: name,
-    };
-    return (
-        <div className="alert alert-primary">
-            <select className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                        <option defaultValue>Currency $ Dollar</option>
-                <option value="Dollar" name="Dollar">$ Dollar</option>
-                <option value="Pound" name="Pound">Pound</option>
-                <option value="Euro" name="Euro">Euro</option>
-                <option value="Ruppee" name="Ruppee">Ruppee</option>
-                  </select>
-        </div>
+    const changeCurrency = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-secondary'> Location {
+      <select name="Location" id="Location" onChange={event=>changeCurrency(event.target.value)}>
+                <option value="₹">$ Dollar</option>
+        <option value="£">£ Pound</option>
+        <option value="€">€ Euro</option>
+        <option value="CAD">Canada(CAD)</option>
+      </select>	
+      }	
+    </div>
     );
 };
-export default Currency;
+
+export default Location;
